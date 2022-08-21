138f8d2a5ae8b110202347289378b5ef4aad5c9f
Glenn Adams <flgadams@gmail.com>
2022-08-21 13:16:29 -0400

    demo pure stretch and stretch with gradient

--- 02images-resizable/StretchViewController.swift
+++ 02images-resizable/StretchViewController.swift
@@ -5,6 +5,9 @@
 //  Created by Glenn Adams on 8/21/22.
 //
 
+// demo: 1 pure stretch
+//       2 stretch with gradient effect 
+
 import UIKit
 
 class StretchViewController: UIViewController {
@@ -13,6 +16,31 @@ class StretchViewController: UIViewController {
         super.viewDidLoad()
 
         self.view.backgroundColor = .systemYellow
+        
+        let marsImage = UIImage(named: "mars")!
+        let marsStretchedImage = marsImage.resizableImage(withCapInsets: .zero, resizingMode: .stretch)
+        
+        
+        let marsImageView = UIImageView(image: marsStretchedImage)
+        marsImageView.contentMode = .scaleToFill
+        self.view.addSubview(marsImageView)
+        marsImageView.frame = CGRect(x: 0, y: 50, width: 350, height: 150)
+        marsImageView.autoresizingMask = .flexibleWidth
+        
+        let marsStretchedImage2 = marsImage.resizableImage(
+            withCapInsets: UIEdgeInsets(top: marsImage.size.height/2.0 - 1,
+                                        left: marsImage.size.width/2.0 - 1,
+                                        bottom: marsImage.size.height/2.0 - 1,
+                                        right: marsImage.size.width/2.0 - 1 ),
+                            resizingMode: .stretch)
+        
+        let marsImageView2 = UIImageView(image: marsStretchedImage2)
+        marsImageView2.contentMode = .scaleAspectFill
+        marsImageView2.clipsToBounds = true
+        
+        self.view.addSubview(marsImageView2)
+        marsImageView2.frame = CGRect(x: 0, y: 400, width: self.view.bounds.width, height: 150)
+        
     }
     
 
--- 02images-resizable/TileViewController.swift
+++ 02images-resizable/TileViewController.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Glenn Adams on 8/21/22.
 //
+// tiling an image without and with cap insets
+//
 
 import UIKit
 
@@ -13,7 +15,29 @@ class TileViewController: UIViewController {
         super.viewDidLoad()
 
         self.view.backgroundColor = .systemTeal
-        // Do any additional setup after loading the view.
+        
+        let marsImage = UIImage(named: "mars")!
+        let marsTiledImage = marsImage.resizableImage(withCapInsets: .zero, resizingMode: .tile)
+        
+        
+        let marsImageView = UIImageView(image: marsTiledImage)
+        marsImageView.contentMode = .scaleToFill
+        self.view.addSubview(marsImageView)
+        marsImageView.frame = CGRect(x: 0, y: 50, width: 250, height: 150)
+        marsImageView.autoresizingMask = .flexibleWidth
+        
+        let marsTiledImage2 = marsImage.resizableImage(withCapInsets: UIEdgeInsets(
+            top: marsImage.size.height/4,
+            left: marsImage.size.width/4,
+            bottom: marsImage.size.height/4,
+            right: marsImage.size.width/4), resizingMode: .tile)
+        
+        let marsImageView2 = UIImageView(image: marsTiledImage2)
+        marsImageView2.contentMode = .scaleToFill
+        self.view.addSubview(marsImageView2)
+        marsImageView2.image = marsTiledImage2
+        marsImageView2.frame = CGRect(x: 0, y: 300, width: 450, height: 150)
+        marsImageView2.autoresizingMask = .flexibleWidth
     }
     
 
